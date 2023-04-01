3e843ffea954bd8d0222d359c318fbb08a75fe5a
Yaroslav Svystun <111186021+eragon2193@users.noreply.github.com>
2023-04-01 22:34:00 +0200

    first commit initial files

---
+++ src/app.js
@@ -0,0 +1,6 @@
+
+
+// To do Tasks:
+// Small header that says Organizer/todolist/smth similar
+// sidebar on the left with buttons like add etc
+// big scrollable list of cards with title and a date that open when you hover over them 
